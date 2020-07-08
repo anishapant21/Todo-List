7f79299b4603ecbd1179e377d292a191b5c90ca7
eternal-moon <anishapant1@gmail.com>
2020-07-08 18:43:07 +0545

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import TodoInput from './Components/TodoInput';
 import TodoList from './Components/TodoList';
-
+import "/home/anisha/git-workspace/todo-list-react/src/fontawesome.js"
 import "bootstrap/dist/css/bootstrap.min.css";
 import {v1 as uuid} from "uuid";
+import { dom } from '@fortawesome/fontawesome-svg-core'
+
+
+dom.watch()
 
 class App extends Component{
   state={
@@ -38,16 +42,28 @@ class App extends Component{
   clearList=()=>{
     this.setState({
       items:[]
-    })
-  }
+    });
+  };
+  handleDelete=(id)=>{
+    const filteredItems=this.state.items.filter(item=> item.id !== id)
+    this.setState({
+      items:filteredItems
+    });
+  };
+
   render(){
     return (
       <div className='container'>
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-4">
           <h3 className="text-capitalize text-center">todo input</h3>
-          <TodoInput item={this.state.item} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
-          <TodoList items={this.state.items} clearList={this.clearList} />
+          <TodoInput item={this.state.item} 
+          handleChange={this.handleChange} 
+          handleSubmit={this.handleSubmit}/>
+          <TodoList items={this.state.items}
+           clearList={this.clearList}
+           handleDelete={this.handleDelete}
+           />
 
           </div>
         </div>
--- src/Components/TodoInput.js
+++ src/Components/TodoInput.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import '/home/anisha/git-workspace/todo-list-react/src/fontawesome.js';
 
 export default class TodoInput extends Component {
     render() {
@@ -8,11 +9,14 @@ export default class TodoInput extends Component {
                 <div className="input-group">
                     <div className="input-group-prepend">
                         <div className="input-group-text bg-primary text-white">
-                            <i className="fas fa-book" />
+                            <i class="fas fa-book"></i>
+                            
 
                         </div>
                     </div>
-                    <input type="text" className="form-control text-capitalize" placeholder="add a todo item" value={item} onChange={handleChange}/>
+                    <input type="text" className="form-control text-capitalize"
+                     placeholder="add a todo item" value={item} 
+                     onChange={handleChange}/>
                 </div>
                 <button type="submit" className="btn btn-block btn-primary mt-3"> add item</button>
             </form>
